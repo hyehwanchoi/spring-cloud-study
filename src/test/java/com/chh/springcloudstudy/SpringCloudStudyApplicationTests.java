609e4e291c6aa0c4399e88a7f9b098059cee6740
chh2644@gmail.com <gpghkschl12#>
2022-07-17 22:38:55 +0900

    Feat: 프로젝트 생성

---
+++ src/test/java/com/chh/springcloudstudy/SpringCloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chh.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
